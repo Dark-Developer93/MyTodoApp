e3d8f45da014a12cb950257d5458735e3863edc7
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2021-07-25 14:21:58 +0200

    chore: change the input to a function based

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,9 +1,34 @@
 import React, { useState } from "react";
 
-const InputTodo = () => {
-  const [title, setTitle] = useState("hello");
-  console.log(useState("hello"));
-  return <div></div>;
+const InputTodo = (props) => {
+  const [title, setTitle] = useState("");
+
+  const onChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      props.addTodoProps(title);
+      setTitle("");
+    } else alert("Please write a todo item");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="What needs to be done?"
+        value={title}
+        name="title"
+        onChange={onChange}
+      />
+      <button type="submit" className="input-submit">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default InputTodo;
