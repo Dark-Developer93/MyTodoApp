4f3d910e44a1da5f722952881166eca8fb872fde
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2021-07-25 14:13:55 +0200

    refactor: change from class to function based

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,44 +1,9 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class InputTodo extends Component {
-  state = {
-    title: "",
-  };
-
-  onChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    if (this.state.title.trim()) {
-      this.props.addTodoProps(this.state.title);
-      this.setState({
-        title: "",
-      });
-    } else alert("Please write a todo item");
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input
-          type="text"
-          className="input-text"
-          placeholder="What needs to be done?"
-          value={this.state.title}
-          name="title"
-          onChange={this.onChange}
-          //   onKeyPress={this.onSubmit}
-        />
-        <button type="submit" className="input-submit">
-          Submit
-        </button>
-      </form>
-    );
-  }
-}
+const InputTodo = () => {
+  const [title, setTitle] = useState("hello");
+  console.log(useState("hello"));
+  return <div></div>;
+};
 
 export default InputTodo;
